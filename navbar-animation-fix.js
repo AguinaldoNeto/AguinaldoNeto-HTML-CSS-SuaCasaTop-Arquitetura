581f1ecafdcbe31be24259c63b333f6b2bf2a954
Aguinaldo Neto <neto_aguinaldo@outlook.com>
2021-03-01 16:51:06 -0300

    Segundo Commit

---
+++ navbar-animation-fix.js
@@ -0,0 +1,9 @@
+$('#collapse-navbar-menu').on('show.bs.collapse', function() 
+		{
+	$('.topcasafina-banner').css('transform', 'translate(-50%, 10%');
+	});
+
+$('#collapse-navbar-menu').on('hide.bs.collapse', function() 
+		{
+	$('.topcasafina-banner').css('transform', 'translate(-50%, -50%');
+	});
